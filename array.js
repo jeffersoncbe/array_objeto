7c3de61f604bb26e28edddaa5c612799ee889aae
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-06-10 20:18:19 -0300

    Iniciando arrays

---
+++ array.js
@@ -0,0 +1,55 @@
+// Conceito de array é: Criar, limpar, adicionar elementos,
+//remover elementos, buscar elementos (filtrar), percorrer os itens.
+let nomes = [
+    'João',
+    'Maria',
+    'José',
+    'Pedro',
+    'Ana',
+    'Carlos',
+
+]
+let carros = []
+
+let idades = [12, '23', '24']
+
+const cidadesArgentina = [
+    'Buenos Aires',
+    'Rosario',
+    'Mendoza',
+    'Cordoba',
+    'Salta',
+    'Santa Fe',
+    'Tucuman',
+    'Entre Rios',
+    'San Juan',
+    'San Luis',
+    'La Rioja',
+    'Catamarca',
+    'Jujuy',
+    'Chaco',
+    'Corrientes',
+    'Formosa',
+    'Santiago del Estero',
+    'Neuquen',
+    'La Pampa',
+    'Santa Cruz',
+    'Tierra del Fuego'
+]
+
+// length retorna o numero com a quantidade de itens da lista
+//console.log(`Tamanho: ${cidadesArgentina.length}`);
+
+//console.log(idades)
+
+// console.log(nomes);
+// nomes.pop()
+// console.log(nomes);
+// console.log(`${nomes[2]} ${nomes[1].toLocaleUpperCase()}`)
+
+
+let frutas = ['abacaxi', 'goiaba', 'manga', 'maracujá', 'kiwi']
+frutas.forEach(function(fruta){
+    console.log(fruta)
+})
+
